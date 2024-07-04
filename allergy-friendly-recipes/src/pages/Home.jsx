5de2cbfe48bb5c9c490fef73d8d34a7e7c0dbbb1
benshabbat <benshabbat27@gmail.com>
2024-07-04 10:40:35 +0300

    home for start

--- allergy-friendly-recipes/src/pages/Home.jsx
+++ allergy-friendly-recipes/src/pages/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getRecipes } from '../services/recipeService';
 
-const Home = () => {
+function Home() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    loadRecipes();
+  }, []);
+
+  const loadRecipes = async () => {
+    const recipeData = await getRecipes();
+    setRecipes(recipeData);
+  };
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1>Allergy Friendly Recipes</h1>
+      <ul>
+        {recipes.map(recipe => (
+          <li key={recipe.id}>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
