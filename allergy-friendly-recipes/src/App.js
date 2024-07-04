b00b3f35039dc9eca9b5661fe97144a743238cb4
benshabbat <benshabbat27@gmail.com>
2024-07-04 10:39:09 +0300

    create some components and pages for start

--- allergy-friendly-recipes/src/App.js
+++ allergy-friendly-recipes/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Recipe from './pages/Recipe';
+import AddRecipe from './pages/AddRecipe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/recipe/:id" component={Recipe} />
+          <Route path="/add-recipe" component={AddRecipe} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
