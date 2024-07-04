f0f77090cd6af2e72110f253830ce04d5ec38d25
benshabbat <benshabbat27@gmail.com>
2024-07-04 18:51:06 +0300

    update

--- allergy-friendly-recipes/client/src/components/RecipeList.jsx
+++ allergy-friendly-recipes/client/src/components/RecipeList.jsx
@@ -1,20 +1,18 @@
-import React, { useMemo } from 'react';
-import { useRecipeContext } from '../contexts/RecipeContext';
+import React, { useState, useEffect } from 'react';
+import { getRecipes } from '../services/api';
 import RecipeItem from './RecipeItem';
 
 function RecipeList() {
-  const { recipes, filter } = useRecipeContext();
+  const [recipes, setRecipes] = useState([]);
 
-  const filteredRecipes = useMemo(() => 
-    recipes.filter(recipe => 
-      !filter.some(allergen => recipe.allergens.includes(allergen))
-    ),
-    [recipes, filter]
-  );
+  useEffect(() => {
+    getRecipes().then(res => setRecipes(res.data));
+  }, []);
 
   return (
-    <div className="recipe-list">
-      {filteredRecipes.map(recipe => (
+    <div>
+      <h2>מתכונים</h2>
+      {recipes.map(recipe => (
         <RecipeItem key={recipe._id} recipe={recipe} />
       ))}
     </div>
