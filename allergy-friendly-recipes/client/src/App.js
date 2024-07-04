089db1daa7304f217d9af6fab93c3b37d82cf93a
benshabbat <benshabbat27@gmail.com>
2024-07-04 14:53:10 +0300

    best parctice

--- allergy-friendly-recipes/client/src/App.js
+++ allergy-friendly-recipes/client/src/App.js
@@ -1,39 +1,23 @@
-import React, { Suspense, lazy } from 'react';
-import { RecipeProvider } from './context/RecipeContext';
-import ErrorBoundary from './components/ErrorBoundary';
+import React from 'react';
+import { RecipeProvider } from './contexts/RecipeContext';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
 import AllergenFilter from './components/AllergenFilter';
 import RecipeSearch from './components/RecipeSearch';
+import { ALLERGENS } from './config';
 import './styles/App.css';
 
-const RecipeList = lazy(() => import('./components/RecipeList'));
-const AddRecipeForm = lazy(() => import('./components/AddRecipeForm'));
-
 function App() {
-  const [filter, setFilter] = React.useState([]);
-  const allergens = ['גלוטן', 'חלב', 'ביצים', 'בוטנים', 'סויה'];
-
-  const handleFilterChange = (allergen, isChecked) => {
-    setFilter(prevFilter => 
-      isChecked 
-        ? [...prevFilter, allergen]
-        : prevFilter.filter(a => a !== allergen)
-    );
-  };
-
   return (
-    <ErrorBoundary>
-      <RecipeProvider>
-        <div className="App">
-          <h1>מתכונים ידידותיים לאלרגיות</h1>
-          <AllergenFilter allergens={allergens} onFilterChange={handleFilterChange} />
-          <RecipeSearch />
-          <Suspense fallback={<div>Loading...</div>}>
-            <RecipeList filter={filter} />
-            <AddRecipeForm />
-          </Suspense>
-        </div>
-      </RecipeProvider>
-    </ErrorBoundary>
+    <RecipeProvider>
+      <div className="App">
+        <h1>מתכונים ידידותיים לאלרגיות</h1>
+        <AllergenFilter allergens={ALLERGENS} />
+        <RecipeSearch />
+        <RecipeList />
+        <AddRecipeForm />
+      </div>
+    </RecipeProvider>
   );
 }
 
