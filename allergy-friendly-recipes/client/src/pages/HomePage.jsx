997533eeeddceb7e45cccfab0e53df884c314e06
benshabbat <benshabbat27@gmail.com>
2024-07-05 01:11:16 +0300

    HomePage

--- allergy-friendly-recipes/client/src/pages/HomePage.jsx
+++ allergy-friendly-recipes/client/src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
-
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMeals } from '../store/slices/mealSlice';
+import AllergyFilter from '../components/AllergyFilter';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   const dispatch = useDispatch();
-  const { meals } = useSelector((state) => state.meals);
+  const { meals, status, error } = useSelector((state) => state.meals);
 
   useEffect(() => {
     dispatch(fetchMeals());
@@ -15,8 +15,11 @@ const HomePage = () => {
   return (
     <div>
       <h1>Welcome to the Meal App</h1>
+      <AllergyFilter />
       <ul>
-        {meals.map((meal) => (
+        {status === 'loading' && <p>Loading meals...</p>}
+        {status === 'failed' && <p>Error: {error}</p>}
+        {status === 'succeeded' && meals.map((meal) => (
           <li key={meal._id}>
             <Link to={`/meals/${meal._id}`}>{meal.name}</Link>
           </li>
