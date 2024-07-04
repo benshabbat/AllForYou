b2334c9169353236a8c5a2c14e0ce69aa641d1d7
benshabbat <benshabbat27@gmail.com>
2024-07-04 14:56:27 +0300

    useReducer

--- allergy-friendly-recipes/client/src/context/RecipeContext.jsx
+++ allergy-friendly-recipes/client/src/context/RecipeContext.jsx
@@ -1,19 +1,39 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
 
 const RecipeContext = createContext();
 
-export const useRecipeContext = () => useContext(RecipeContext);
+const initialState = {
+  recipes: [],
+  filter: [],
+  loading: false,
+  error: null
+};
+
+function recipeReducer(state, action) {
+  switch (action.type) {
+    case 'SET_RECIPES':
+      return { ...state, recipes: action.payload, loading: false };
+    case 'SET_FILTER':
+      return { ...state, filter: action.payload };
+    case 'ADD_RECIPE':
+      return { ...state, recipes: [...state.recipes, action.payload] };
+    case 'SET_LOADING':
+      return { ...state, loading: action.payload };
+    case 'SET_ERROR':
+      return { ...state, error: action.payload, loading: false };
+    default:
+      return state;
+  }
+}
 
 export const RecipeProvider = ({ children }) => {
-  const [recipes, setRecipes] = useState([]);
-  const [filter, setFilter] = useState([]);
+  const [state, dispatch] = useReducer(recipeReducer, initialState);
 
-  const value = {
-    recipes,
-    setRecipes,
-    filter,
-    setFilter
-  };
-
-  return <RecipeContext.Provider value={value}>{children}</RecipeContext.Provider>;
+  return (
+    <RecipeContext.Provider value={{ state, dispatch }}>
+      {children}
+    </RecipeContext.Provider>
+  );
 };
+
+export const useRecipeContext = () => useContext(RecipeContext);
