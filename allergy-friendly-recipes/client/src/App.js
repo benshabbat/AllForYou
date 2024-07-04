30b567c7167930bf5603f8c2aa616a9407523d15
benshabbat <benshabbat27@gmail.com>
2024-07-04 13:37:43 +0300

    update app.js

--- allergy-friendly-recipes/client/src/App.js
+++ allergy-friendly-recipes/client/src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import AllergenFilter from './components/AllergenFilter';
 
 function App() {
+  const [filter, setFilter] = useState([]);
+  const allergens = ['גלוטן', 'חלב', 'ביצים', 'בוטנים', 'סויה'];
+
+  const handleFilterChange = (allergen, isChecked) => {
+    if (isChecked) {
+      setFilter([...filter, allergen]);
+    } else {
+      setFilter(filter.filter(a => a !== allergen));
+    }
+  };
+
+  const handleAddRecipe = (recipe) => {
+    // כאן נוסיף לוגיקה לשמירת המתכון בשרת
+    console.log('מתכון חדש:', recipe);
+  };
+
   return (
     <div className="App">
       <h1>מתכונים ידידותיים לאלרגיות</h1>
+      <AllergenFilter allergens={allergens} onFilterChange={handleFilterChange} />
+      <RecipeList filter={filter} />
+      <AddRecipeForm onAddRecipe={handleAddRecipe} />
     </div>
   );
 }
