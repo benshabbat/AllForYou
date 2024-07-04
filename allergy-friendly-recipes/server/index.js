528df096317b0a51210fdbbca80391c7482b61e7
benshabbat <benshabbat27@gmail.com>
2024-07-05 00:37:30 +0300

    ci cd

--- allergy-friendly-recipes/server/index.js
+++ allergy-friendly-recipes/server/index.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import connectDB from './config/db.js';
-import dotenv from 'dotenv';
-import mealRoutes from './routes/mealRoutes.js';
-import userRoutes from './routes/userRoutes.js';
+import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import mealRoutes from "./routes/mealRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import logger from "./config/logger.js";
 
 dotenv.config();
 
@@ -12,8 +13,10 @@ connectDB();
 
 app.use(express.json());
 
-app.use('/api/meals', mealRoutes);
-app.use('/api/users', userRoutes);
-
+app.use("/api/meals", mealRoutes);
+app.use("/api/users", userRoutes);
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
