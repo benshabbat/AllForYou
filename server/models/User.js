150f5d199ef61fba183fdf3dbd78ba74b9cfc609
benshabbat <benshabbat27@gmail.com>
2024-07-07 20:50:57 +0300

    Enhance User model with comments
    - Add explanatory comments to User schema
    - Clarify password hashing middleware function
    - Explain password comparison method

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
+// הגדרת סכמת המשתמש
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -26,13 +27,18 @@ const UserSchema = new mongoose.Schema({
 });
 
 // האש לסיסמה לפני שמירה
+// מופעל אוטומטית לפני שמירת מסמך חדש או עדכון סיסמה
 UserSchema.pre('save', async function(next) {
+  // בדיקה אם הסיסמה שונתה
   if (!this.isModified('password')) return next();
+  
+  // יצירת האש לסיסמה
   this.password = await bcrypt.hash(this.password, 12);
   next();
 });
 
 // מתודה להשוואת סיסמאות
+// מאפשרת השוואה בין סיסמה מוצפנת לסיסמה שהוזנה
 UserSchema.methods.comparePassword = async function(candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
