458f5a55d60151a606243f111e5801e514df29e3
benshabbat <benshabbat27@gmail.com>
2024-07-08 21:46:16 +0300

    Add missing route and update routing structure
    - Add "/my-recipes" route for user's personal recipes
    - Ensure all routes are properly defined and protected
    - Improve code organization and readability
    - Add comments for better understanding of routing structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider, useDispatch, useSelector } from 'react-redux';
-import { store } from './store/index.js';
+import { store } from './store/index.js'; 
 import { loadUser } from './store/slices/authSlice';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -10,15 +10,14 @@ import AddRecipe from './pages/AddRecipe';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import UserProfile from './pages/UserProfile';
+import MyRecipes from './pages/MyRecipes'; // יש להוסיף קומפוננטה זו אם היא לא קיימת
 import PrivateRoute from './components/PrivateRoute';
 
-// קומפוננטה פנימית המכילה את תוכן האפליקציה
 function AppContent() {
   const dispatch = useDispatch();
   const { token } = useSelector((state) => state.auth);
 
   useEffect(() => {
-    // טעינת פרטי המשתמש אם קיים טוקן
     if (token) {
       dispatch(loadUser());
     }
@@ -28,18 +27,21 @@ function AppContent() {
     <Router>
       <Header />
       <Routes>
+        {/* נתיבים ציבוריים */}
         <Route path="/" element={<Home />} />
         <Route path="/recipes" element={<RecipeList />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        
+        {/* נתיבים מאובטחים */}
         <Route path="/add-recipe" element={<PrivateRoute><AddRecipe /></PrivateRoute>} />
         <Route path="/profile" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
+        <Route path="/my-recipes" element={<PrivateRoute><MyRecipes /></PrivateRoute>} /> {/* נתיב חדש */}
       </Routes>
     </Router>
   );
 }
 
-// קומפוננטה ראשית המספקת את ה-Redux store לכל האפליקציה
 function App() {
   return (
     <Provider store={store}>
