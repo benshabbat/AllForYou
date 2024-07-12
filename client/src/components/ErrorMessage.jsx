b24acde84eb1710ac1bf3e6b85db59c03dfaebca
benshabbat <benshabbat27@gmail.com>
2024-07-12 16:14:12 +0300

    improve for err

--- client/src/components/ErrorMessage.jsx
+++ client/src/components/ErrorMessage.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ErrorMessage.module.css';
 
-function ErrorMessage({ message }) {
+function ErrorMessage({ message, type = 'error' }) {
   return (
-    <div className={styles.error}>
-      <p className={styles.message}>{message}</p>
+    <div className={`${styles.message} ${styles[type]}`}>
+      <p>{message}</p>
     </div>
   );
 }
 
 ErrorMessage.propTypes = {
   message: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['error', 'warning', 'info']),
 };
 
 export default ErrorMessage;
