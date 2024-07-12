ed0b0a1e8bd190da2e90fccd72863d94b3b263ff
benshabbat <benshabbat27@gmail.com>
2024-07-12 16:16:56 +0300

    update loading

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Loading.module.css';
 
-function Loading({ message = 'טוען...' }) {
+function Loading({ message = 'טוען...', size = 'medium', fullScreen = false }) {
+  const containerClass = fullScreen ? styles.fullScreenContainer : styles.container;
+  const spinnerClass = `${styles.spinner} ${styles[size]}`;
+
   return (
-    <div className={styles.loading}>
-      <div className={styles.spinner}></div>
-      <p className={styles.message}>{message}</p>
+    <div className={containerClass}>
+      <div className={spinnerClass}></div>
+      {message && <p className={styles.message}>{message}</p>}
     </div>
   );
 }
 
 Loading.propTypes = {
   message: PropTypes.string,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  fullScreen: PropTypes.bool,
 };
 
 export default Loading;
