c495089ca1716fe0b21140001957bf568d285a71
benshabbat <benshabbat27@gmail.com>
2024-07-12 16:15:05 +0300

    update skeleton

--- client/src/components/Skeleton.jsx
+++ client/src/components/Skeleton.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Skeleton.module.css';
 
-const Skeleton = ({ width, height }) => (
+const Skeleton = ({ width, height, borderRadius, className }) => (
   <div 
-    className={styles.skeleton} 
-    style={{ width, height }}
+    className={`${styles.skeleton} ${className}`} 
+    style={{ width, height, borderRadius }}
   />
 );
+
+Skeleton.propTypes = {
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  borderRadius: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  className: PropTypes.string,
+};
+
+Skeleton.defaultProps = {
+  width: '100%',
+  height: '20px',
+  borderRadius: '4px',
+};
 
 export default Skeleton;
