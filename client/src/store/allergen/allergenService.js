2c703513a5253e0caacc54a9ea23ff10d2d67d20
benshabbat <benshabbat27@gmail.com>
2024-07-28 17:36:25 +0300

    allergenService

--- client/src/store/allergen/allergenService.js
+++ client/src/store/allergen/allergenService.js
@@ -1,1 +1,4 @@
 import api from '../../services/api';
+
+export const fetchAllergens = () => api.get('/allergens');
+export const fetchAllergensByIds = (ids) => api.get('/allergens/byIds', { params: { ids: ids.join(',') } });
