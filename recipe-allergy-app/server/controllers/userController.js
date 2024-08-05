682110ed806564f12ab89b4118df53f74cafa2cc
benshabbat <benshabbat27@gmail.com>
2024-08-05 17:16:20 +0300

    login

---
+++ recipe-allergy-app/server/controllers/userController.js
@@ -0,0 +1,56 @@
+import User from '../models/userModel.js';
+
+export const register = async (req, res) => {
+  try {
+    const { username, email, password, allergies } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400).json({ message: "User already exists" });
+      return;
+    }
+
+    const user = await User.create({
+      username,
+      email,
+      password,
+      allergies,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        allergies: user.allergies,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+export const login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      const user = await User.findOne({ email });
+  
+      if (user && (await user.matchPassword(password))) {
+        res.json({
+          _id: user._id,
+          username: user.username,
+          email: user.email,
+          allergies: user.allergies
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid email or password' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Server error' });
+    }
+  }
