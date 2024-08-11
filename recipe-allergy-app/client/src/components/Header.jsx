2a26803f8733b109ea0bd48a07579466e3eae5c5
benshabbat <benshabbat27@gmail.com>
2024-08-11 15:16:48 +0300

    Update Header to use Redux state and actions

--- recipe-allergy-app/client/src/components/Header.jsx
+++ recipe-allergy-app/client/src/components/Header.jsx
@@ -1,14 +1,32 @@
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../redux/slices/userSlice';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const { userInfo } = useSelector((state) => state.user);
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <header>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/recipes">Recipes</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
+          {userInfo ? (
+            <>
+              <li>Welcome, {userInfo.username}!</li>
+              <li><button onClick={handleLogout}>Logout</button></li>
+            </>
+          ) : (
+            <>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/register">Register</Link></li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
