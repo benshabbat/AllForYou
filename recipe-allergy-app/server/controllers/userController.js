c60521e748371f8555a5eb7134e293bb6fa9a54d
benshabbat <benshabbat27@gmail.com>
2024-08-12 15:13:28 +0300

    add first and last name

--- recipe-allergy-app/server/controllers/userController.js
+++ recipe-allergy-app/server/controllers/userController.js
@@ -1,8 +1,9 @@
-import User from '../models/userModel.js';
+import User from "../models/userModel.js";
 
 export const register = async (req, res) => {
   try {
-    const { username, email, password, allergies } = req.body;
+    const { username, fitsName, lastName, email, password, allergies } =
+      req.body;
 
     const userExists = await User.findOne({ email });
 
@@ -13,6 +14,8 @@ export const register = async (req, res) => {
 
     const user = await User.create({
       username,
+      fitsName,
+      lastName,
       email,
       password,
       allergies,
@@ -22,6 +25,8 @@ export const register = async (req, res) => {
       res.status(201).json({
         _id: user._id,
         username: user.username,
+        fitsName:user.fitsName,
+        lastName:user.lastName,
         email: user.email,
         allergies: user.allergies,
       });
@@ -33,24 +38,25 @@ export const register = async (req, res) => {
   }
 };
 
-
 export const login = async (req, res) => {
-    try {
-      const { email, password } = req.body;
-  
-      const user = await User.findOne({ email });
-  
-      if (user && (await user.matchPassword(password))) {
-        res.json({
-          _id: user._id,
-          username: user.username,
-          email: user.email,
-          allergies: user.allergies
-        });
-      } else {
-        res.status(401).json({ message: 'Invalid email or password' });
-      }
-    } catch (error) {
-      res.status(500).json({ message: 'Server error' });
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (user && (await user.matchPassword(password))) {
+      res.status(201).json({
+        _id: user._id,
+        username: user.username,
+        fitsName:user.fitsName,
+        lastName:user.lastName,
+        email: user.email,
+        allergies: user.allergies,
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
     }
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
   }
+};
