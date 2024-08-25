4ed80eeb1cededab23ad55d3f9c2edfd130066e2
benshabbat <benshabbat27@gmail.com>
2024-08-25 12:34:22 +0300

    Add Profile route to App component

--- recipe-allergy-app/client/src/App.jsx
+++ recipe-allergy-app/client/src/App.jsx
@@ -7,7 +7,7 @@ import Home from './pages/Home';
 import Recipes from './pages/Recipes';
 import Login from './pages/Login';
 import Register from './pages/Register';
-
+import Profile from './pages/Profile';
 function App() {
   return (
     <Provider store={store}>
@@ -20,6 +20,7 @@ function App() {
               <Route path="/recipes" element={<Recipes />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/profile" element={<Profile />} />
             </Routes>
           </main>
         </div>
