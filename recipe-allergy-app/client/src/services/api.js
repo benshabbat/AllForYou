c6e943ca31c6bc03f72fedaa394cf959082de0e3
benshabbat <benshabbat27@gmail.com>
2024-08-25 12:34:53 +0300

    Add Profile route to App component

--- recipe-allergy-app/client/src/services/api.js
+++ recipe-allergy-app/client/src/services/api.js
@@ -11,3 +11,16 @@ export const register = async (username, email, password) => {
   const response = await axios.post(`${API_URL}/users/register`, { username, email, password });
   return response.data;
 };
+
+
+export const updateUserProfile = async (userData) => {
+    const token = JSON.parse(localStorage.getItem('userInfo')).token;
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await axios.put(`${API_URL}/users/profile`, userData, config);
+    return response.data;
+  };
